28fe58e60d9e51b5ae48133561e62f8e7078a835
Griffin <griffinstorback@gmail.com>
2021-06-01 12:06:09 -0700

    updated apps section

--- src/data/apps.js
+++ src/data/apps.js
@@ -3,8 +3,8 @@ const data = [
     {
         title: "MessageMap",
         subtitle: "Available for free on the App Store",
-        link: "https://www.apple.com",
-        image: "/images/me_icon.jpeg",
+        link: "https://apps.apple.com/app/messagemap/id1517249971",
+        image: "/images/messagemap_icon.png",
         date: "2020-09-17",
         desc: `
             MessageMap is anonymous, location based forum. Find a message
@@ -14,12 +14,14 @@ const data = [
     },
     {
         title: "WotMovie",
-        subtitle: "SOON TO BE Available for free on the App Store",
-        link: "https://www.apple.com",
-        image: "/images/me_icon.jpeg",
-        date: "TBA",
+        subtitle: "Available for free on the App Store",
+        link: "https://apps.apple.com/app/wotmovie/id1555955570",
+        image: "/images/wotmovie_icon.png",
+        date: "2021-05-01",
         desc: `
-            WotMovie is... currently unfinished.
+            WotMovie is a Movie/TV Show trivia game. Given a description,
+            start typing the name and select the correct answer - or if you
+            don't know the answer, reveal it and discover something new!
         `
     }
 ];
